d771766593c844d73a9c0778cb6df98ed32c6bac
Jordan <jordan.donguy@gmail.com>
2025-04-18 15:05:09 +0200

    Set up user register, login and authentication

---
+++ passport-config.js
@@ -0,0 +1,31 @@
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (user == null) {
+            return done(null, false, { message: 'No user found with that email' })
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: 'The password is incorrect' })
+            }
+        } catch(err) {
+            return done(err)
+        }
+    };
+
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser));
+    passport.serializeUser((user, done) => {
+        return done(null, user.id)
+    });
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+};
+
+module.exports = initialize;
