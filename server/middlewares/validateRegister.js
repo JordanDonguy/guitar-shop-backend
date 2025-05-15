e79e3d4b6576c64d019fd73c8af285eccc768e6b
Jordan <jordan.donguy@gmail.com>
2025-05-15 23:05:46 +0200

    Create validateRegister, validateLogin and handleValidation middlewares (using express-validator)

---
+++ server/middlewares/validateRegister.js
@@ -0,0 +1,24 @@
+const { body, validationResult } = require("express-validator");
+
+const validateRegister = [
+  // User infos validations
+  body("email").isEmail().withMessage("Invalid email format").normalizeEmail(),
+  body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters"),
+  body("first_name").trim().notEmpty().withMessage("First name is required"),
+  body("last_name").trim().notEmpty().withMessage("Last name is required"),
+  body("phone_number").optional({ checkFalsy: true }).isMobilePhone().withMessage("Invalid phone number"),
+
+  // Address validations
+  body("street").trim().notEmpty().withMessage("Street address is required"),
+  body("city").trim().notEmpty().withMessage("City is required"),
+  body("state").trim().notEmpty().withMessage("State is required"),
+  body("postal_code")
+    .trim()
+    .notEmpty()
+    .withMessage("Postal code is required")
+    .isPostalCode("any")
+    .withMessage("Invalid postal code"),
+  body("country").trim().notEmpty().withMessage("Country is required"),
+];
+
+module.exports = validateRegister
