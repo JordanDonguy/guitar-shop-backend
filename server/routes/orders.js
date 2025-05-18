24a72af8b0ef87c2c72203021a74d9a0caef27ed
Jordan <jordan.donguy@gmail.com>
2025-05-18 23:09:10 +0200

    Modify /orders route and add a /orders/items route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -8,22 +8,33 @@ const {
 
 router.get("/", checkAuthenticated, async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.query.userId;
+    if (!userId || userId != req.user.id) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
 
     const orders = await getOrdersByUserId(userId);
-
-    if (orders && orders.length > 0) {
-      await Promise.all(
-        orders.map(async (order) => {
-          const items = await getItemsByOrderId(order.id);
-          order.items = items;
-        }),
-      );
-    }
 
     return res.status(200).json({ orders });
   } catch (error) {
     console.error("GET /orders/", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/items", checkAuthenticated, async (req, res) => {
+  try {
+    const userId = req.query.userId;
+    const order_id = req.query.order_id;
+
+    if (!userId || userId != req.user.id) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+
+    const items = await getItemsByOrderId(order_id);
+    return res.status(200).json({ items });
+  } catch (error) {
+    console.error("GET /orders/items", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
